54372ac9ea54eb25a80d31effd6823a65646ed21
HuzvN <53541149+HuzvN@users.noreply.github.com>
2019-10-22 19:22:07 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,36 +1,58 @@
-const Discord = require("discord.js");
-const bot = new Discord.Client();
-const moment = require('moment');
-const client = new Discord.Client();
-const db = require('quick.db');
-require('moment-duration-format');
+const { Client, Collection } = require("discord.js");
+const { config } = require("dotenv");
 
+const client = new Client({
+    disableEveryone: true
+})
 
-let prefix = "vk!";//botun ön eki 
-let owner = "351395168588660746";// sizin id'niz
+// Collections
+client.commands = new Collection();
+client.aliases = new Collection();
 
-bot.on("ready", guild => {
-    bot.user.setActivity('Zahiti', { type: 'WATCHING'  })
-  .then(presence => console.log(`Activity set to ${presence.game ? presence.game.name : 'none'}`))
-  .catch(console.error);
-    console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Şu an ` + bot.channels.size + ` adet kanala, ` + bot.guilds.size + ` adet sunucuya ve ` + bot.guilds.reduce((a, b) => a + b.memberCount, 0).toLocaleString() + ` kullanıcıya hizmet veriliyor!`);
-    console.log("Bağlandım!")
+config({
+    path: __dirname + "/.env"
 });
 
-bot.on('message', msg => {
-  if (msg.content.startsWith(prefix + "başlat")) {
-    const exampleEmbed = new Discord.RichEmbed()
-	      .setColor('#0099ff')
-	      .setTitle('Vampir Köylü Başlıyor Katılmak için emojiye bas.')
-      	.setTimestamp()
-
-    msg.channel.send(exampleEmbed);
-    msg.react("🎉");
-    
-
-    
-    }
+// Run the command loader
+["command"].forEach(handler => {
+    require(`./handlers/${handler}`)(client);
 });
 
+client.on("ready", () => {
+    console.log(`Hi, ${client.user.username} is now online!`);
 
-bot.login(process.env.TOKEN);
+    client.user.setPresence({
+        status: "online",
+        game: {
+            name: "me getting developed",
+            type: "WATCHING"
+        }
+    }); 
+})
+
+client.on("message", async message => {
+    const prefix = "_";
+
+    if (message.author.bot) return;
+    if (!message.guild) return;
+    if (!message.content.startsWith(prefix)) return;
+
+    // If message.member is uncached, cache it.
+    if (!message.member) message.member = await message.guild.fetchMember(message);
+
+    const args = message.content.slice(prefix.length).trim().split(/ +/g);
+    const cmd = args.shift().toLowerCase();
+    
+    if (cmd.length === 0) return;
+    
+    // Get the command
+    let command = client.commands.get(cmd);
+    // If none is found, try to find it by alias
+    if (!command) command = client.commands.get(client.aliases.get(cmd));
+
+    // If a command is finally found, run the command
+    if (command) 
+        command.run(client, message, args);
+});
+
+client.login(process.env.TOKEN);
